b82b2c0e84524de8d2c682e297b22d4dcd905b0c
nirzargandhi <54696036+nirzargandhi@users.noreply.github.com>
2025-03-07 19:08:54 +0530

    Commit

---
+++ CalendarViewDemo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CalendarViewDemo
+//
+//  Created by Nirzar Gandhi on 30/07/24.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
